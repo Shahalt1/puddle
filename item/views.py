b1e892077cb750e01a8eb4e94493e51500ffbcc8
Shahal Thayyil <Shahalthayyil2020@gmail.com>
2024-10-27 14:51:13 +0530

    second commit

--- item/views.py
+++ item/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Item
 
 # Create your views here.
+
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_item = Item.objects.filter(is_sold=False, category=item.category).exclude(
+        pk=pk
+    )[:3]
+    context = {"item": item, "related_item": related_item}
+    return render(request, "item/detail.html", context)
